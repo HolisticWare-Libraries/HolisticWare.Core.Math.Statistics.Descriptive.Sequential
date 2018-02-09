13d8bd753102aeac49eb56bb9ab30f6700b00e8d
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-02-09 23:51:09 +0100

    Frequencies refactoring

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/Sequential/ArrayList/IEnumerableExtensions.Frequencies.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/Sequential/ArrayList/IEnumerableExtensions.Frequencies.cs
@@ -15,28 +15,7 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         [Obsolete("Performance issue (boxing)")]
         public static IEnumerable<KeyValuePair<object, uint>> Frequencies(this System.Collections.ArrayList x)
         {
-            long n = x.Count;
-
-            Dictionary<object, uint> frequencies = new Dictionary<object, uint>((int)n);
-
-            //for (int i = 0; i < n; i++)
-            System.Threading.Tasks.Parallel.For
-                        (
-                            0,
-                            n,
-                            i =>
-                            {
-                                object x_i = x.ElementAt((int)i);
-                                if (frequencies.ContainsKey(x_i))
-                                {
-                                    frequencies[x_i] += 1;
-                                }
-                                else
-                                {
-                                    frequencies.Add(x_i, 1);
-                                }
-                            }
-                        );
+            Dictionary<object, uint> frequencies = x.FrequencyCounter();
 
             IEnumerable<KeyValuePair<object, uint>> frequencies_sorted =
                 from pair in frequencies
