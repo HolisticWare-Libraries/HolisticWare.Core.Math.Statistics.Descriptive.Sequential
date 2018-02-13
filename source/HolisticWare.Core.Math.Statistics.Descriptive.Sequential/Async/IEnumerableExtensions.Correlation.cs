75864e53059147c0bdfe0adefdb75f488e3f42c2
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-02-13 18:39:47 +0100

    Correlation static API + some fixes

---
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/Async/IEnumerableExtensions.Correlation.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Core.Math.Statistics.Descriptive.Sequential.Async
+{
+	public class IEnumerableExtensions
+	{
+	}
+}
--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/Sequential/IEnumerableExtensions.Correlation.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/Sequential/IEnumerableExtensions.Correlation.cs
@@ -13,45 +13,21 @@ namespace Core.Math.Statistics.Descriptive.Sequential
     /// <see cref="http://www.mathsisfun.com/data/correlation.html"/>
     public static partial class IEnumerableExtensionsCorrelation
     {
-        public static double CorrelationOptimized(this IEnumerable<double> x, IEnumerable<double> y)
-        {
-            int n = x.Count();
-
-            double sum_x = 0.0;
-            double sum_y = 0.0;
-            double sum_x_pow_2 = 0.0;
-            double sum_y_pow_2 = 0.0;
-            double sum_product_x_y = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                double x_i = x.ElementAt(i);
-                double y_i = y.ElementAt(i);
-
-                sum_x += x_i;
-                sum_y += y_i;
-                sum_x_pow_2 += x_i * x_i;
-                sum_y_pow_2 += y_i * y_i;
-                sum_product_x_y += x_i * y_i;
-            }
-
-            double correlation =
-                (n * sum_product_x_y - sum_x * sum_y)
-                /
-                (
-                    System.Math.Sqrt((n * sum_x_pow_2) - sum_x * sum_x)
-                    *
-                    System.Math.Sqrt((n * sum_y_pow_2) - sum_y * sum_y)
-                )
-                ;
-
-            return correlation;
-        }
-
         public static double Correlation(this IEnumerable<short> x, IEnumerable<short> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if( System.Math.Abs(standard_deviation_x) < Double.Epsilon )
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -75,7 +51,18 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         public static double Correlation(this IEnumerable<ushort> x, IEnumerable<ushort> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -99,7 +86,18 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         public static double Correlation(this IEnumerable<int> x, IEnumerable<int> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -123,7 +121,18 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         public static double Correlation(this IEnumerable<uint> x, IEnumerable<uint> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -148,7 +157,18 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         public static double Correlation(this IEnumerable<long> x, IEnumerable<long> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -172,7 +192,18 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         public static double Correlation(this IEnumerable<ulong> x, IEnumerable<ulong> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -193,10 +224,56 @@ namespace Core.Math.Statistics.Descriptive.Sequential
                 ;        
         }
 
+        public static double Correlation(this IEnumerable<float> x, IEnumerable<float> y)
+        {
+            double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
+            double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
+
+            double sum = 0.0;
+
+            int n = x.Count();
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += x.ElementAt(i) * y.ElementAt(i);
+            }
+
+            double x_mean_average_arithmetic = x.Average();
+            double y_mean_average_arithmetic = y.Average();
+
+            return
+                (sum - n * x_mean_average_arithmetic * y_mean_average_arithmetic)
+                /
+                ((n - 1) * standard_deviation_x * standard_deviation_y)
+                ;
+        }
+
         public static double Correlation(this IEnumerable<double> x, IEnumerable<double> y)
         {
             double standard_deviation_x = x.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_x) < Double.Epsilon)
+            {
+                return 0;
+            }
+
             double standard_deviation_y = y.StandardDeviationSample();
+
+            if (System.Math.Abs(standard_deviation_y) < Double.Epsilon)
+            {
+                return 0;
+            }
 
             double sum = 0.0;
 
@@ -220,7 +297,18 @@ namespace Core.Math.Statistics.Descriptive.Sequential
         public static decimal Correlation(this IEnumerable<decimal> x, IEnumerable<decimal> y)
         {
             decimal standard_deviation_x = x.StandardDeviationSample();
+
+            if (standard_deviation_x == 0.0M)
+            {
+                return 0;
+            }
+
             decimal standard_deviation_y = y.StandardDeviationSample();
+
+            if (standard_deviation_y == 0.0M)
+            {
+                return 0;
+            }
 
             decimal sum = 0.0M;
 
