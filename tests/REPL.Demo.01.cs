78e27ac7b056a0b6ef061f049eb2c46e34cd892c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-02-21 23:52:23 +0100

    REPL demo

--- tests/REPL.Demo.01.cs
+++ tests/REPL.Demo.01.cs
@@ -1,8 +1,15 @@
+Loa
 // namespace inclusion (using) necessary for LINQ Extension methods
 using Core.Math.Statistics.Descriptive.Sequential;
+
 
 ShowUsing (); 
 
 List<int> data01 = new List<int>(){1,2,4,5};
 
 double mean_arithmetic_01 = data01.MeanArithmetic();
+Console.WriteLine($"mean_arithmetic_01 = {mean_arithmetic_01}");  
+
+double mean_geometric_01 = data01.MeanGeometric();
+Console.WriteLine($"mean_arithmetic_01 = {mean_geometric_01}");  
+
