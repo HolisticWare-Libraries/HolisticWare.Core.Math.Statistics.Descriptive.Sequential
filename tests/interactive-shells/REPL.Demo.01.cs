33fb617517eae128fb7acef55f2b1bb346ea7dcd
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-02-24 18:27:10 +0100

    interactive scripts

--- tests/interactive-shells/REPL.Demo.01.cs
+++ tests/interactive-shells/REPL.Demo.01.cs
@@ -10,23 +10,13 @@ using Core.Math.Statistics.Descriptive.Sequential;
 Stopwatch sw = null;
 
 //----------------------------------------------------------------------------------------------------
-List<int> data01 = new List<int>(){1,2,4,5};
-
-sw = Stopwatch.StartNew();
-double mean_arithmetic_01 = data01.MeanArithmetic();
-Console.WriteLine($"mean_arithmetic_01 = {mean_arithmetic_01}");  
-sw.Stop();
-Console.WriteLine($"List<int>.Average() size={data01.Count} elapsed[ticks]={sw.ElapsedTicks}");
-sw.Reset();
-
-sw = Stopwatch.StartNew();
-double mean_geometric_01 = data01.MeanGeometric();
-sw.Stop();
-Console.WriteLine($"List<int>.Average() size={data01.Count} elapsed[ticks]={sw.ElapsedTicks}");
-sw.Reset();
-//----------------------------------------------------------------------------------------------------
-
-List<int> data02 = new List<int> { 2, 4, 3, 5, 6, 7, 4, 4, 2, 1 };
+List<int> data01 = new List<int>()
+{
+    1,
+    2,
+    4,
+    5
+};
 
 //----------------------------------------------------------------------------------------------------
 sw = Stopwatch.StartNew();
@@ -35,5 +25,22 @@ sw.Stop();
 Console.WriteLine($"List<int>.Average() size={data01.Count} elapsed[ticks]={sw.ElapsedTicks}");
 sw.Reset();
 //----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
+sw = Stopwatch.StartNew();
+double mean_arithmetic_01 = data01.MeanArithmetic();
+Console.WriteLine($"mean_arithmetic_01 = {mean_arithmetic_01}");  
+sw.Stop();
+Console.WriteLine($"List<int>.Average() size={data01.Count} elapsed[ticks]={sw.ElapsedTicks}");
+sw.Reset();
+
+//----------------------------------------------------------------------------------------------------
+sw = Stopwatch.StartNew();
+double mean_geometric_01 = data01.MeanGeometric();
+sw.Stop();
+Console.WriteLine($"List<int>.Average() size={data01.Count} elapsed[ticks]={sw.ElapsedTicks}");
+sw.Reset();
+//----------------------------------------------------------------------------------------------------
+
 
 
--- tests/interactive-shells/REPL.Demo.01.csharp-w-load-assembly.cs
+++ tests/interactive-shells/REPL.Demo.01.csharp-w-load-assembly.cs
@@ -1,17 +1,55 @@
+LoadAssembly("../..source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential.NetStandard10/bin/Debug/netstandard1.0/HolisticWare.Core.Math.Statistics.Descriptive.Sequential.dll"); 
 
-LoadAssembly("../../source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential.NetStandard10/bin/Debug/netstandard1.0/HolisticWare.Core.Math.Statistics.Descriptive.Sequential.dll");
+// Stopwatch
+using System.Diagnostics;
 
 // namespace inclusion (using) necessary for LINQ Extension methods
 using Core.Math.Statistics.Descriptive.Sequential;
 
+//ShowUsing (); 
 
-ShowUsing (); 
+//----------------------------------------------------------------------------------------------------
+List<int> data01 = new List<int>()
+{
+    1,
+    2,
+    4,
+    5
+};
 
-List<int> data01 = new List<int>(){1,2,4,5};
+Stopwatch sw = null;
 
+//----------------------------------------------------------------------------------------------------
+sw = Stopwatch.StartNew();
+double average = data01.Average();
+sw.Stop();
+Console.WriteLine($"List<int>.MeanArithmetic()");
+Console.WriteLine($"          mean_arithmetic    = {average}");
+Console.WriteLine($"          size               = {data01.Count}");
+Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+sw.Reset();
+//----------------------------------------------------------------------------------------------------
+sw = Stopwatch.StartNew();
 double mean_arithmetic_01 = data01.MeanArithmetic();
-Console.WriteLine($"mean_arithmetic_01 = {mean_arithmetic_01}");  
-
+Console.WriteLine($"List<int>.MeanArithmetic()");
+Console.WriteLine($"          mean_arithmetic    = {mean_arithmetic_01}");
+Console.WriteLine($"          size               = {data01.Count}");
+Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+sw.Stop();
+sw.Reset();
+//----------------------------------------------------------------------------------------------------
+sw = Stopwatch.StartNew();
 double mean_geometric_01 = data01.MeanGeometric();
-Console.WriteLine($"mean_arithmetic_01 = {mean_geometric_01}");  
+sw.Stop();
+Console.WriteLine($"List<int>.MeanArithmetic()");
+Console.WriteLine($"          mean_arithmetic    = {mean_geometric_01}");
+Console.WriteLine($"          size               = {data01.Count}");
+Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+sw.Reset();
+//----------------------------------------------------------------------------------------------------
+
+
 
