09ee8c3b4964a5e89ab8ce8ba1f5b07fb05f1b5e
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-03-04 22:50:56 +0100

    Percentiles algorithm

---
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/Sync/IEnumerableExtensions.Percentile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Core.Math.Statistics.Descriptive.Sequential;
+
+namespace Core.Math.Statistics.Descriptive.Sequential
+{
+    /// <summary>
+    /// Correlation
+    /// </summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Percentile"/>
+    /// <see cref="https://www.emathhelp.net/calculators/probability-statistics/percentile-calculator"/>
+    /// <see cref="https://www.emathhelp.net/calculators/probability-statistics/percentile-class-rank-calculator"/> 
+    public static partial class IEnumerableExtensionsPercentiles
+    {
+        //==============================================================================================================
+
+        //--------------------------------------------------------------------------------------------------------------
+        public static double PercentileRank(this IEnumerable<int> x, int p)
+        {
+            if (p < 0 || p > 100)
+            {
+                throw new ArgumentOutOfRangeException($"Percentile must be in the range [0, 1] the vale was {p}");
+            }
+            int n = x.Count();
+
+            return p / 100 * (n + 1);
+        }
+
+        public static (int lower, double interpolated, int upper) Percentile(this IEnumerable<int> x, int p)
+        {
+            if (p < 0  || p > 100)
+            {
+                throw new ArgumentOutOfRangeException($"Percentile must be in the range [0, 1] the vale was {p}");
+            }
+
+            IEnumerable<int> x_sorted = x.OrderBy(x_i => x_i);
+
+            int n = x.Count();
+            double index_percentile = p / 100.0 * (n + 1);
+
+            int index_lower = (int)index_percentile;
+            int index_upper = (int)(index_percentile + 1.0);
+            double fraction = index_percentile - index_lower;
+            int x_lower = x.ElementAt(index_lower - 1);
+            int x_upper = x.ElementAt(index_upper - 1);
+            double x_interpolated = fraction * (x_upper - x_lower) + x_lower;
+
+            return (x_lower, x_interpolated, x_upper);
+        }
+        //--------------------------------------------------------------------------------------------------------------
+
+        //==============================================================================================================
+
+    }
+}
