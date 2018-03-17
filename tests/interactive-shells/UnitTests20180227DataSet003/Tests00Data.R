4809acb4da01bb99b7a6401f91389df2fad38cab
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-03-17 19:09:02 +0100

    R scripts for tests initial commit

---
+++ tests/interactive-shells/UnitTests20180227DataSet003/Tests00Data.R
@@ -0,0 +1,10 @@
+
+
+
+#! /usr/bin/env Rscript
+
+# Rscript  Tests00Data.R
+
+data01 <- c( 1, 2, 2, 3, 3, 4, 5, 6 )
+print(data01)
+
---
+++ tests/interactive-shells/UnitTests20180227DataSet003/Tests01MeanAverageMedian.R
@@ -0,0 +1,20 @@
+
+#source(Tests00Data)
+source("Tests00Data.R")
+
+#Tests00Data
+
+mean_arithmetic <- mean(data01)
+print(mean_arithmetic)
+
+#mean_geometric = mean(data01, "g");
+#mean_geometric
+#mean_harmonic = mean(data01, "h");
+#mean_harmonic
+#mean_squared = meansq(data01);
+#mean_squared
+
+#median = median(data01);
+#median
+
+
