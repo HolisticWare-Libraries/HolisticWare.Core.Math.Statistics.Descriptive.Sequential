a3be9186985d775e811651e6a67826ba452e0dbd
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-03-20 22:52:57 +0100

    tests update

---
+++ tests/unit-tests/UnitTests.CommonShared/Sync/FromFiles/UnitTests20180318DataSetRand50Samp02/Tests01MeanAverageMedian.cs
@@ -0,0 +1,152 @@
+﻿// /*
+//    Copyright (c) 2017-12
+//
+//    moljac
+//    Test.cs
+//
+//    Permission is hereby granted, free of charge, to any person
+//    obtaining a copy of this software and associated documentation
+//    files (the "Software"), to deal in the Software without
+//    restriction, including without limitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the
+//    Software is furnished to do so, subject to the following
+//    conditions:
+//
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+// */
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+#if XUNIT
+using Xunit;
+using Test = Xunit.FactAttribute;
+#elif NUNIT
+using NUnit.Framework;
+using Fact=NUnit.Framework.TestAttribute;
+#elif MSTEST
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using Fact = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+#endif
+
+using Core.Math.Statistics.Descriptive.Sequential;
+
+namespace UnitTests.Core.Math.Statistics.Descriptive.Sequential.Sync
+{
+    public partial class UnitTests20180318DataSetRand50Samp02
+    {
+        [Test]
+        public void MeanArithmetic()
+        {
+            //====================================================================================================
+            //  Arrange
+            //  reading data from files
+
+            sw = Stopwatch.StartNew();
+
+            //----------------------------------------------------------------------------------------------------
+            // Act
+            double mean_arithmetic = Data.MeanArithmetic();
+            sw.Stop();
+            Console.WriteLine($"List<double>.MeanArithmetic()");
+            Console.WriteLine($"          mean_arithmetic    = {mean_arithmetic}");
+            Console.WriteLine($"          size               = {Data.Count()}");
+            Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+            Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+            sw.Reset();
+
+            //----------------------------------------------------------------------------------------------------
+            // Assert
+            #if NUNIT
+            Assert.AreEqual(177.6258, mean_arithmetic, 0.00001);
+            #elif XUNIT
+            Assert.Equal(177.6258, mean_arithmetic, 5);
+            #elif MSTEST
+            #endif
+            //====================================================================================================
+
+            return;
+        }
+
+        [Test]
+        public void MeanGeometric()
+        {
+            //====================================================================================================
+            //  Arrange
+            //  reading data from files
+
+            sw = Stopwatch.StartNew();
+
+            //----------------------------------------------------------------------------------------------------
+            // Act
+            double mean_geometric = Data.MeanGeometric();
+            sw.Stop();
+            Console.WriteLine($"List<double>.MeanGeometric()");
+            Console.WriteLine($"          mean_geometric     = {mean_geometric}");
+            Console.WriteLine($"          size               = {Data.Count()}");
+            Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+            Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+            sw.Reset();
+
+            //----------------------------------------------------------------------------------------------------
+            // Assert
+            #if NUNIT
+            Assert.That(!Double.IsInfinity(mean_geometric));
+            Assert.AreEqual(176.5979, mean_geometric, 0.0001);
+            #elif XUNIT
+            Assert.True(Double.IsInfinity(mean_geometric));
+            Assert.Equal(176.5979, mean_geometric, 4);
+            #elif MSTEST
+            Assert.IsTrue(!Double.IsInfinity(mean_geometric));
+            Assert.AreEqual(176.5979, mean_geometric, 0.0001);
+            #endif
+            //====================================================================================================
+
+            return;
+        }
+
+        [Test]
+        public void MeanGeometric_Exception()
+        {
+            #if NUNIT
+            Assert.That
+                    (
+                        () =>
+                        {
+                            // TODO: System.InvalidCastException : Specified cast is not valid.
+                            decimal mean_deomatric_decimal = (Data.Select(x_i => (decimal)x_i)).MeanGeometric();
+                        },
+                        Throws.InstanceOf(typeof(System.OverflowException))
+                    );
+            #elif XUNIT
+            // System.OverflowException : Arithmetic operation resulted in an overflow.
+            Assert.Throws<System.OverflowException>
+                    (
+                        () =>
+                        {
+                            // TODO: System.InvalidCastException : Specified cast is not valid.
+                            decimal mean_deomatric_decimal = (Data.Select(x_i => (decimal)x_i)).MeanGeometric();
+                        }
+                    );
+            #elif MSTEST
+            #endif
+
+            return;
+        }
+    }
+}
---
+++ tests/unit-tests/UnitTests.CommonShared/Sync/x01-Correlation-Covariance/FromFilesRandSamples.cs
@@ -0,0 +1,155 @@
+﻿// /*
+//    Copyright (c) 2017-12
+//
+//    moljac
+//    Test.cs
+//
+//    Permission is hereby granted, free of charge, to any person
+//    obtaining a copy of this software and associated documentation
+//    files (the "Software"), to deal in the Software without
+//    restriction, including without limitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the
+//    Software is furnished to do so, subject to the following
+//    conditions:
+//
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+// */
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.IO;
+using UnitTests.CommonShared.Sync.FromFiles.UnitTests20180318DataSetBasketball;
+
+#if XUNIT
+using Xunit;
+using Test = Xunit.FactAttribute;
+using OneTimeSetUp = System.ObsoleteAttribute;
+#elif NUNIT
+using NUnit.Framework;
+using Fact=NUnit.Framework.TestAttribute;
+#elif MSTEST
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OneTimeSetUp = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using Fact = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+#endif
+
+using Core.Math.Statistics.Descriptive.Sequential;
+
+namespace UnitTests.Core.Math.Statistics.Descriptive.Sequential.Sync
+{
+    public partial class FromFilesRandSamples
+    {
+        Stopwatch sw = null;
+        List<double> data01 = null;
+        List<double> data02 = null;
+
+        double? correlation_data01_data02_01 = null;
+        double? correlation_data02_data01_01 = null;
+
+        [Test]
+        public void Correlation_01_02()
+        {
+            //====================================================================================================
+            //  Arrange
+            //  reading data from files
+            data01 = UnitTests20180318DataSetRand50Samp01.Data;
+            data02 = UnitTests20180318DataSetRand50Samp02.Data;
+
+            sw = Stopwatch.StartNew();
+
+            //----------------------------------------------------------------------------------------------------
+            // Act
+            correlation_data01_data02_01 = data01.Correlation(data02);
+            sw.Stop();
+            Console.WriteLine($"List<double>.Correlation(List<double>)");
+            Console.WriteLine($"          correlation        = {correlation_data01_data02_01}");
+            Console.WriteLine($"          size               = {data01.Count()} x {data02.Count()}");
+            Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+            Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+            sw.Reset();
+
+            //----------------------------------------------------------------------------------------------------
+            // Assert
+            #if NUNIT
+            Assert.AreEqual(0.1882, correlation_data01_data02_01, 0.0001);
+            #elif XUNIT
+            Assert.Equal(0.1882, (double)correlation_data01_data02_01, 4);
+            #elif MSTEST
+            #endif
+            //====================================================================================================
+
+            return;
+        }
+
+        [Test]
+        public void Correlation_02_01()
+        {
+            //====================================================================================================
+            //  Arrange
+            //  reading data from files
+
+            sw = Stopwatch.StartNew();
+
+            //----------------------------------------------------------------------------------------------------
+            // Act
+            correlation_data02_data01_01 = data02.Correlation(data01);
+            sw.Stop();
+            Console.WriteLine($"List<double>.Correlation(List<double>)");
+            Console.WriteLine($"          correlation        = {correlation_data02_data01_01}");
+            Console.WriteLine($"          size               = {data01.Count()} x {data02.Count()}");
+            Console.WriteLine($"          elapsed[ticks]     = {sw.ElapsedTicks}");
+            Console.WriteLine($"          elapsed[ms]        = {sw.Elapsed.TotalMilliseconds}");
+            sw.Reset();
+
+            //----------------------------------------------------------------------------------------------------
+            // Assert
+            #if NUNIT
+            Assert.AreEqual(0.1882, correlation_data02_data01_01, 0.0001);
+            #elif XUNIT
+            Assert.Equal(0.1882, (double)correlation_data02_data01_01, 4);
+            #elif MSTEST
+            #endif
+            //====================================================================================================
+
+            #if NUNIT
+            Assert.AreEqual
+                        (
+                            (double)correlation_data01_data02_01,
+                            (double)correlation_data02_data01_01,
+                            0.00001
+                        );
+            #elif XUNIT
+            Assert.Equal
+                        (
+                            (double)correlation_data01_data02_01,
+                            (double)correlation_data02_data01_01,
+                            5
+                        );
+            #elif MSTEST
+            Assert.AreEqual
+                        (
+                            (double)correlation_data01_data02_01,
+                            (double)correlation_data02_data01_01,
+                            0.00001
+                        );
+            #endif
+
+            return;
+        }
+    }
+}
