a61aebf3ebbd98553b8eabf6dee6536ac0bf8506
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-05-05 22:13:12 +0200

    tests 02 added

--- tests/unit-tests/UnitTests.CommonShared/Sync/DarkVaderTests/Tests20180119Dataset02/Tests00Data.cs
+++ tests/unit-tests/UnitTests.CommonShared/Sync/DarkVaderTests/Tests20180119Dataset02/Tests00Data.cs
@@ -68,7 +68,9 @@ namespace UnitTests.Core.Math.Statistics.Descriptive.Sequential.Sync
     [TestClass] // for MSTest - NUnit [TestFixture] and XUnit not needed
     public partial class Tests20180119Dataset02
     {
-        static double[] data01 = new[]
+        Stopwatch sw = null;
+
+        static double[] data = new[]
         {
             25.4, 14.7, 16.4, 15.3, 17.4, 15.0, 15.0, 19.4, 17.5, 14.7,
             18.0, 15.2, 16.0, 13.7, 18.2, 15.9, 14.7, 13.3, 15.1, 17.6,
@@ -77,9 +79,28 @@ namespace UnitTests.Core.Math.Statistics.Descriptive.Sequential.Sync
             12.9, 10.5, 11.3, 14.3, 19.5, 18.6, 18.6, 22.9, 19.8, 13.4,
             18.2, 14.5, 24.0, 16.5, 16.4, 28.9, 13.5, 13.6, 11.9, 18.2,
         };
-        static double[] weights = Enumerable.Repeat(1.0, data01.Length).ToArray();
+        static double[] weights = Enumerable.Repeat(1.0, data.Length).ToArray();
 
 
-        Stopwatch sw = null;
+        static double[] data_array = null;
+        static ArraySegment<double> data_array_segment;
+        static List<double> data_list = null;
+        static Stack<double> data_stack = null;
+        static Queue<double> data_queue = null;
+        static ObservableCollection<double> data_observable_collection = null;
+        static LinkedList<double> data_linked_list = null;
+
+        static Tests20180119Dataset02()
+        {
+            data_array = data.ToArray();
+            data_array_segment = new ArraySegment<double>(data);
+            data_list = new List<double>(data);
+            data_queue = new Queue<double>(data);
+            data_stack = new Stack<double>(data);
+            data_linked_list = new LinkedList<double>(data);
+            data_observable_collection = new ObservableCollection<double>(data);
+
+            return;
+        }
     }
 }
