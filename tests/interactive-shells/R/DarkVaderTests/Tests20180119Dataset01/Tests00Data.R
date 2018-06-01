88cd8906c35b75f7570b4b3f66d532ea4a3dc9e0
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-06-01 23:37:59 +0200

    R scripts for inclusion

---
+++ tests/interactive-shells/R/DarkVaderTests/Tests20180119Dataset01/Tests00Data.R
@@ -0,0 +1,12 @@
+#! /usr/bin/env Rscript
+
+# Rscript  Tests00Data.R
+# R -e 'source("./Tests00Data.R")'
+data <- c( 
+1, 2, 2, 3, 3, 4, 5, 6 
+)
+print(data)
+
+setwd("./")
+sys.source("Tests011MeasuresCentralTendencies.R")
+sys.source("Tests011MeasuresDistribution.R")
