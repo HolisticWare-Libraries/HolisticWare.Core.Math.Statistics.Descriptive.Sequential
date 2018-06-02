a166dac0edc66c60b1863708899198d2c66ebfbe
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-06-02 06:59:59 +0200

    R script path reading

--- tests/interactive-shells/R/DarkVaderTests/Tests20180119Dataset01/Tests00Data.R
+++ tests/interactive-shells/R/DarkVaderTests/Tests20180119Dataset01/Tests00Data.R
@@ -1,5 +1,10 @@
 #! /usr/bin/env Rscript
 
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
+
 # Rscript  Tests00Data.R
 # R -e 'source("./Tests00Data.R")'
 data <- c( 
@@ -7,6 +12,16 @@ data <- c(
 )
 print(data)
 
+other.name <- paste(sep="/", script.basename, "Tests011MeasuresCentralTendencies.R")
+print(paste("Sourcing",other.name,"from",script.name))
+source(other.name)
+
+
+# returns the path of the current script file. It works after the script was saved.
+# script.dir <- dirname(sys.frame(1)$ofile)
+
+dir()
 setwd("./")
-sys.source("Tests011MeasuresCentralTendencies.R")
+dir()
+
 sys.source("Tests011MeasuresDistribution.R")
--- tests/interactive-shells/R/DarkVaderTests/Tests20180119Dataset01/Tests011MeasuresCentralTendencies.R
+++ tests/interactive-shells/R/DarkVaderTests/Tests20180119Dataset01/Tests011MeasuresCentralTendencies.R
@@ -1,4 +1,12 @@
-setwd("./")
-sys.source("Tests00Data.R")
+#! /usr/bin/env Rscript
+
+#initial.options <- commandArgs(trailingOnly = FALSE)
+#file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
+
+other.name <- paste(sep="/", script.basename, "Tests00Data.R ")
+print(paste("Sourcing",other.name,"from",script.name))
+source(other.name)
 
 print(data)
