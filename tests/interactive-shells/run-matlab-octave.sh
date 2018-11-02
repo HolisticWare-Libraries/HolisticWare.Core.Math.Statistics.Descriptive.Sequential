e56d3e7f62584b77388cad8cdddd9c282eeb05e3
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-11-02 13:45:14 +0100

    matlab octave shell script

---
+++ tests/interactive-shells/run-matlab-octave.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export EXECUTABLE=octave
+
+$EXECUTABLE \
+    ./interactive-shells/matlab-octave/01-DarkVaderTests/Tests20180119Dataset01/Tests00Data.m
+
+$EXECUTABLE \
+    ./interactive-shells/matlab-octave/01-DarkVaderTests/Tests20180119Dataset01/Tests011MeasuresCentralTendencies.m 
+
