4fdc00d322186d045a1965f4fb8ce4c0da5f8d51
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-11-13 21:39:20 +0100

    csv reading

--- tests/interactive-shells/python/FromFiles/UnitTests20180318DataSetBasketball/Tests00Data.py
+++ tests/interactive-shells/python/FromFiles/UnitTests20180318DataSetBasketball/Tests00Data.py
@@ -3,6 +3,30 @@
 import os
 import sys
 
+#-------------------------------------------------------------------------------------------
+# https://docs.python.org/3/library/csv.html
+import csv
+csv_filename = '../../../../../externals/Core.Math.Samples/data/HolisticWare.Core.Sample.Data/Xtras-SampleData/Basketball.comma.csv';
+with open(csv_filename) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            print(f'{"      ".join(row)}')
+            line_count += 1
+        else:
+            print(row)
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+print("======================================================================================")
+# https://realpython.com/python-csv/
+import pandas
+data_pandas = pandas.read_csv(csv_filename)
+print(data_pandas)
+#-------------------------------------------------------------------------------------------
+
 data = \
 [ \
     1, 2, 2, 3, 3, 3, 3, 4, 4, 5 \
