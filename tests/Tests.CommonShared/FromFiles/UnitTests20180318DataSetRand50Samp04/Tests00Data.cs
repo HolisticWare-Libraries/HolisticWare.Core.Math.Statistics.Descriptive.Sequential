97d3597aab812583ef5660171fa52b28422ba2bb
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-11-16 20:58:16 +0100

    flattened folder structure

---
+++ tests/Tests.CommonShared/FromFiles/UnitTests20180318DataSetRand50Samp04/Tests00Data.cs
@@ -0,0 +1,130 @@
+﻿// /*
+//    Copyright (c) 2017-12
+//
+//    moljac
+//    Test.cs
+//
+//    Permission is hereby granted, free of charge, to any person
+//    obtaining a copy of this software and associated documentation
+//    files (the "Software"), to deal in the Software without
+//    restriction, including without limitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the
+//    Software is furnished to do so, subject to the following
+//    conditions:
+//
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+// */
+
+#if XUNIT
+using Xunit;
+// NUnit aliases
+using Test = Xunit.FactAttribute;
+using OneTimeSetUp = HolisticWare.Core.Testing.UnitTests.UnitTestsCompatibilityAliasAttribute;
+// XUnit aliases
+using TestClass = HolisticWare.Core.Testing.UnitTests.UnitTestsCompatibilityAliasAttribute;
+using TestContext = HolisticWare.Core.Testing.UnitTests.TestContext;
+#elif NUNIT
+using NUnit.Framework;
+// MSTest aliases
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestProperty = NUnit.Framework.PropertyAttribute;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+// XUnit aliases
+using Fact = NUnit.Framework.TestAttribute;
+#elif MSTEST
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// NUnit aliases
+using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using OneTimeSetUp = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+// XUnit aliases
+using Fact = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+#endif
+
+#if BENCHMARKDOTNET
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Attributes.Jobs;
+#else
+using Benchmark = HolisticWare.Core.Testing.BenchmarkTests.Benchmark;
+using ShortRunJob = HolisticWare.Core.Testing.BenchmarkTests.ShortRunJob;
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+using Core.Math.Statistics.Descriptive.Sequential;
+
+namespace UnitTests.Core.Math.Statistics.Descriptive.Sequential.Sync
+{
+    [TestClass] // for MSTest - NUnit [TestFixture] and XUnit not needed
+    public partial class UnitTests20180318DataSetRand50Samp04
+    {
+        static List<double> data01 = null;
+
+        Stopwatch sw = null;
+
+        // [OneTimeSetUp] // for MSTest - ClassInitialize - public, static, void
+        public static void LoadDataFromFile()
+        {
+            #if NUNIT
+            string directory_test = TestContext.CurrentContext.TestDirectory;
+            #elif XUNIT
+            string directory_test = Environment.CurrentDirectory;
+            #elif MSTEST
+            string directory_test = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            #endif
+
+            string path_data = null;
+            string text = null;
+            string[] lines = null;
+
+            //------------------------------------------------------------------
+            path_data = System.IO.Path.Combine
+                                    (
+                                        new string[]
+                                            {
+                                                directory_test,
+                                                $@"Xtras-SampleData",
+                                                $@"Rand_Samp4_50.csv",
+                                            }
+                                    );
+            using (StreamReader reader = new StreamReader(path_data))
+            {
+                text = reader.ReadToEnd();
+            }
+            lines = text.Split
+                            (
+                                new string[] { Environment.NewLine },
+                                StringSplitOptions.RemoveEmptyEntries
+                            );
+
+            data01 = new List<double>();
+            foreach (string s in lines)
+            {
+                double data_item = Double.Parse(s);
+                data01.Add(data_item);
+            }
+            //------------------------------------------------------------------
+
+            return;
+        }
+    }
+}
