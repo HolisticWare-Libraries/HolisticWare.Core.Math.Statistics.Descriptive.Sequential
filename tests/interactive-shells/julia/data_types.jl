0214ce3f2318348e98d6de6b5bdedc504a289bec
katodix <darko.katovic@gmail.com>
2020-10-05 16:05:00 +0200

    line changes

--- tests/interactive-shells/julia/data_types.jl
+++ tests/interactive-shells/julia/data_types.jl
@@ -3,4 +3,13 @@ data = CSV.read("Pejcic_318.csv", copycols = true)
 
 # eltypes(data)
 
-eltype.(eachcol(data))
+# eltype.(eachcol(data))
+
+
+
+#fname = "Pejcic_318.csv"
+#data = readtable(fname, separator = ',')
+typeof(data) # DataFrame
+size(data) # (1599,12)
+
+
