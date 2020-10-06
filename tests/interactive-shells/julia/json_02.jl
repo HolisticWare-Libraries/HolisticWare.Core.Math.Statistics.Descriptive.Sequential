7f08f0b90318beaa5293a6a6bbccb016c004e3bc
moljac <mcvjetko@holisticware.net>
2020-10-06 12:09:17 +0200

    mode

--- tests/interactive-shells/julia/json_02.jl
+++ tests/interactive-shells/julia/json_02.jl
@@ -1,7 +1,9 @@
 import Pkg; 
+Pkg.add("JSON")
 Pkg.add("JSON3")
 Pkg.add("LazyJSON")
 
+import JSON;
 import JSON3;
 import LazyJSON;
 
@@ -12,16 +14,34 @@ end
 
 a2 = JSON3.read(dicttxt)
 
-println(a2)
-println(a1)
+println("a2 = ", a2)
+println("a1 = ", a1)
 
 
-import Pkg; 
 Pkg.add("Statistics")
-using Statistics; 
+Pkg.add("StatsBase")
+using Statistics;
+using StatsBase;
+
+# https://rosettacode.org/wiki/Averages/Arithmetic_mean#Julia
 
 #m1 = mean(a1)
-m2 = mean(a2)
+mean_arithmetic_2 = mean(a2)
+mode_2 = StatsBase.mode(a2)
 
 #println(m1)
-println("mean arithmetic = ", m2)
+println("mean arithmetic (m2)   = ", mean_arithmetic_2)
+println("mode (m2)              = ", mode_2)
+
+
+dict_result = Dict(
+                    "mean arithmetic" => mean_arithmetic_2, 
+                    "mode" => mode_2
+                )
+string_data_results = JSON.json(dict_result)
+
+# write the file with the stringdata variable information
+open("results.json", "w") do f
+    write(f, string_data_results)
+end
+                
