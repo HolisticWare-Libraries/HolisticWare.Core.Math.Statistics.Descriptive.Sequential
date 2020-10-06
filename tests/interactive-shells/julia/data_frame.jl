74f72b2224e9bbd1363ddadf5a3e1d3010aa8dc7
katodix <darko.katovic@gmail.com>
2020-10-06 10:51:29 +0200

    Update data_frame

--- tests/interactive-shells/julia/data_frame.jl
+++ tests/interactive-shells/julia/data_frame.jl
@@ -1,5 +1,7 @@
-# fname = "Pejcic_318.csv" 
-# data = readdlm(fname, separator = ',')                               
+fname = "../../../externals/Core.Math.Samples/data/Pejcic_318.csv"
+data = readdlm(fname, separator = ',')
+
+print(data)
 # using DataFrames, CSV, Statistics, StatsBase
 # data = CSV.read("Pejcic_318.csv", header=true, delim=',', copycols = true)
 
