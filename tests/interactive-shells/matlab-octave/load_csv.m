249a033f9975d40c7f509a6aca0e4799cf0d184f
katodix <darko.katovic@gmail.com>
2020-10-06 17:55:33 +0200

    Create load_csv.m

---
+++ tests/interactive-shells/matlab-octave/load_csv.m
@@ -0,0 +1,19 @@
+# pkg install -forge io
+
+pkg load io
+
+# if "OpenJDK 64-Bit Server VM warning..." then javaMethod ("getProperty", "java.lang.System", "MyProperty");
+
+addpath('../../../externals/Core.Math.Samples/data')
+
+d = csv2cell('Pejcic_318.csv')
+
+d(1, :) %
+
+d(2:end, :) %
+
+# dlmread ('Pejcic_318.csv',',',2,0)
+
+# csvread('Pejcic_318.csv')
+
+# data = csvread('Pejcic_318.csv')
