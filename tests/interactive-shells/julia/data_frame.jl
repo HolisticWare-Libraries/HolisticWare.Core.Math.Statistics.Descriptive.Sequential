99fc5d21c940914a356a872225a23c4ed0265924
katodix <darko.katovic@gmail.com>
2020-10-06 10:46:47 +0200

    data correction

--- tests/interactive-shells/julia/data_frame.jl
+++ tests/interactive-shells/julia/data_frame.jl
@@ -4,16 +4,3 @@
 # data = CSV.read("Pejcic_318.csv", header=true, delim=',', copycols = true)
 
 # println((data),"\n")
-
-#-----------------------------------------------------------------------------------------------
-# 1st run - packages installation
-
-#import Pkg; 
-#Pkg.add("HypothesisTests")
-#Pkg.add("Random")
-
-using HypothesisTests, Random 
-
-Random.seed!(0); x= randn(100); y= randn(100);
-
-OneSampleTTest(x,y)
