5709edd0729dec2bcd62bbad4ae935afb2da9fc5
katodix <darko.katovic@gmail.com>
2020-10-07 21:08:27 +0200

    octave statistics with dataframe

--- tests/interactive-shells/matlab-octave/load_csv.m
+++ tests/interactive-shells/matlab-octave/load_csv.m
@@ -1,21 +1,56 @@
-# pkg install -forge io
-
-pkg load io
+# https://octave.sourceforge.io
 
 # javaMethod ("getProperty", "java.lang.System", "MyProperty");
 
 # if "OpenJDK 64-Bit Server VM warning..." then: javaMethod ("getProperty", "java.lang.System", "MyProperty");
 
-addpath('/Users/katodix/Projects/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/externals/Core.Math.Samples/data')
+# pkg install -forge io
+# pkg install -forge statistics
+# pkg install -forge nan
+# pkg install -forge dataframe
 
-data = csv2cell('Pejcic_318.csv')
+pkg load io statistics nan dataframe
 
-data(1, :) %
+addpath('../../../externals/Core.Math.Samples/data')
 
-data(2:end, :) %
+# data = csv2cell('Pejcic_318.csv')
+# data(1, :) %
+# data(2:end, :) %
 
-# dlmread ('Pejcic_318.csv',',',2,0)
+# dlmread ('Pejcic_318.csv',',',1,5)
 
-csvread('Pejcic_318.csv')
+# csvread('Pejcic_318.csv')
+# data = csvread('Pejcic_318.csv')
 
-data = csvread('Pejcic_318.csv')
+mean(data)
+median(data)
+mode(data)
+range(data)
+var(data)
+std(data)
+skewness(data)
+kurtosis(data)
+
+statistics(data)
+
+#############
+# DataFrame #
+#############
+data = dataframe('Pejcic_318.csv')
+
+# prvi arg.: od kojeg do kojeg retka; drugi arg.: koje varijable
+#df = data(1:318, ["ATV"; "ATT"; "AOP"; "ANN"; "MKUS"; "MPOL"; "MP20"; "MPRR"; "MTAP"; "MSDM"; "MDTR"; "MVIS"])
+
+class(df)
+df.colnames
+size(df)
+columns(df)
+rows(df)
+
+data(:, 5:8)
+df1 = data(:, 5:8)
+summary(df1)
+
+df2 = data(1:318, ["ATV"; "ATT"])
+mean(df.ATV)
+statistics(df.ATV)
