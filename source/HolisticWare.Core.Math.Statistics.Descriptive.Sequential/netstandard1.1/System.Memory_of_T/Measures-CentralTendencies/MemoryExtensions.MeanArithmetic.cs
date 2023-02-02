75d98a1b5aef9b68df82a47ba76dca5050c583b7
moljac <mcvjetko@holisticware.net>
2023-02-02 20:53:21 +0100

    source fixes

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.1/System.Memory_of_T/Measures-CentralTendencies/MemoryExtensions.MeanArithmetic.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.1/System.Memory_of_T/Measures-CentralTendencies/MemoryExtensions.MeanArithmetic.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+
 using Core.Math.Statistics.Descriptive;
 
 namespace Core.Math.Statistics.Descriptive
