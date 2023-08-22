a1813d6b707d033a1a980b80d82a388186593c95
moljac <mcvjetko@holisticware.net>
2023-08-22 21:12:39 +0200

    Update WelchStatistics.cs

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T/Methods-Inferential/WelchStatistics.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T/Methods-Inferential/WelchStatistics.cs
@@ -9,95 +9,146 @@ namespace Core.Math.Statistics.Descriptive
     /// <summary>
     /// Correlation
     /// </summary>
-    public static partial class WelchtStatistic
+    public static partial class
+                                        WelchtStatistic
     {
         //==============================================================================================================
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<byte> x, 
-                                                                            IEnumerable<byte> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<byte> x,
+                                            IEnumerable<byte> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<short> x, 
-                                                                            IEnumerable<short> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<short> x,
+                                            IEnumerable<short> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<ushort> x, 
-                                                                            IEnumerable<ushort> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<ushort> x,
+                                            IEnumerable<ushort> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<int> x, 
-                                                                            IEnumerable<int> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<int> x,
+                                            IEnumerable<int> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<uint> x, 
-                                                                            IEnumerable<uint> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<uint> x,
+                                            IEnumerable<uint> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<long> x, 
-                                                                            IEnumerable<long> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<long> x,
+                                            IEnumerable<long> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<ulong> x, 
-                                                                            IEnumerable<ulong> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<ulong> x,
+                                            IEnumerable<ulong> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<float> x, 
-                                                                            IEnumerable<float> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<float> x,
+                                            IEnumerable<float> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (double welch_t, double degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<double> x, 
-                                                                            IEnumerable<double> y
-                                                                        )
+        public static
+            (
+                double welch_t,
+                double degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<double> x,
+                                            IEnumerable<double> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
 
-        public static (decimal welch_t, decimal degrees_of_freedom) Calculate
-                                                                        (
-                                                                            IEnumerable<decimal> x, 
-                                                                            IEnumerable<decimal> y
-                                                                        )
+        public static
+            (
+                decimal welch_t,
+                decimal degrees_of_freedom
+            )
+                                        Calculate
+                                        (
+                                            IEnumerable<decimal> x,
+                                            IEnumerable<decimal> y
+                                        )
         {
             return x.WelchtStatistic(y);
         }
