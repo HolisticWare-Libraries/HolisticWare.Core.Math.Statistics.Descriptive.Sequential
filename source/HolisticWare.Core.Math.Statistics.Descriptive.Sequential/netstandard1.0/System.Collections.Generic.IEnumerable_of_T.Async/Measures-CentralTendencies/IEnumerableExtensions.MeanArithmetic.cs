e7b52853d7d0ab32dd7da741e3023b10a6898a0b
moljac <mcvjetko@holisticware.net>
2023-08-22 21:12:30 +0200

    Update IEnumerableExtensions.MeanArithmetic.cs

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MeanArithmetic.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MeanArithmetic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 using Core.Math.Statistics.Descriptive;
 
@@ -11,7 +12,8 @@ namespace Core.Math.Statistics.Descriptive
     /// Mean (Average) Arithmetic
     /// </summary>
     /// <see href="https://en.wikipedia.org/wiki/Average"/>
-    public static partial class IEnumerableExtensionsMeanArithmetic
+    public static partial class
+                                        IEnumerableExtensionsMeanArithmetic
     {
         //==============================================================================================================
         /// <summary>
@@ -29,111 +31,241 @@ namespace Core.Math.Statistics.Descriptive
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<byte> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<byte> x
+                                        )
         {
-            return x.Select(x_i => (int)x_i).Average();
-        }
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-        #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]  
-        #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<ushort> x
-                                    )
-        {
-            return x.Select(x_i => (int)x_i).Average();
-        }
-
-        #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<short> x
-                                    )
-        {
-            return x.Select(x_i => (int)x_i).Average();
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<int> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<ushort> x
+                                        )
         {
-            return x.Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<uint> x
-                                    )        
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<short> x
+                                        )
         {
-            return x.Select(x_i => (long)x_i).Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<long> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<int> x
+                                        )
         {
-            return x.Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<ulong> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<uint> x
+                                        )
         {
-            return x.Select(x_i => (double)x_i).Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<float> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<long> x
+                                        )
         {
-            return x.Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static double MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<double> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<ulong> x
+                                        )
         {
-            return x.Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
 
         #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         #endif
-        public static decimal MeanArithmeticAsync
-                                    (
-                                        this IEnumerable<decimal> x
-                                    )
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<float> x
+                                        )
         {
-            return x.Average();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
+        }
+
+        #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        #endif
+        public static
+            Task<double>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<double> x
+                                        )
+        {
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
+        }
+
+        #if METHODIMPL_OPTIONS_AGRESSIVE_INLINING
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        #endif
+        public static
+            Task<decimal>
+                                        MeanArithmeticAsync
+                                        (
+                                            this IEnumerable<decimal> x
+                                        )
+        {
+            TaskCompletionSource<decimal> tcs;
+            tcs = new TaskCompletionSource<decimal>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            decimal result = x.MeanArithmetic();
+                            tcs.SetResult(result);
+                        }
+                    );
+
+            return tcs.Task;
         }
         //==============================================================================================================
     }
