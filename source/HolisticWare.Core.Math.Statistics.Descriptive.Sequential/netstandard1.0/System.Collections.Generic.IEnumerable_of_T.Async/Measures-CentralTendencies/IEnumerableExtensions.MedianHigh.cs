b43f4e829a6ae2b7947d8c229fd44de7191c5d56
moljac <mcvjetko@holisticware.net>
2023-08-23 07:57:01 +0200

    Update IEnumerableExtensions.MedianHigh.cs

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MedianHigh.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MedianHigh.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Core.Math.Statistics.Descriptive
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
-    public static partial class IEnumerableExtensionsMedianHigh
+    public static partial class
+                                        IEnumerableExtensionsMedianHigh
     {
         //==============================================================================================================
         public static double MedianHighAsync
