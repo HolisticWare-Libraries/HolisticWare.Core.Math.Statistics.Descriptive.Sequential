13792dfc7ae4db1f7e01a734c6fc46e70cc63b76
moljac <mcvjetko@holisticware.net>
2023-08-26 23:56:18 +0200

    Update IEnumerableExtensions.MeanGeneralized.cs

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MeanGeneralized.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MeanGeneralized.cs
@@ -16,194 +16,225 @@ namespace Core.Math.Statistics.Descriptive
                                         IEnumerableExtensionsMeanGeneralized
     {
         //==============================================================================================================
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<byte> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                byte x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<short> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                short x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<ushort> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                ushort x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<int> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                int x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<uint> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                uint x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<long> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                long x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<ulong> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                ulong x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<float> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                float x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static double MeanGeneralizedAsync
+        public static 
+            Task<double> 
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<double> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                double x_i = x.ElementAt(i);
-                sum += System.Math.Pow(x_i, power);
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
-        public static decimal MeanGeneralizedAsync
+        public static 
+            Task<decimal> 
+             
+                                        MeanGeneralizedAsync
                                         (
                                             this IEnumerable<decimal> x,
                                             double power
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<decimal> tcs;
+            tcs = new TaskCompletionSource<decimal>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            decimal result = x.MeanGeneralized(power);
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            decimal sum = 0.0M;
-
-            for (int i = 0; i < n; i++)
-            {
-                decimal x_i = x.ElementAt(i);
-                sum += (decimal) System.Math.Pow((double)x_i, power);
-            }
-
-            return (decimal) System.Math.Pow( (double) sum / n, 1.0 / power);
+            return tcs.Task;
         }
 
         //==============================================================================================================
