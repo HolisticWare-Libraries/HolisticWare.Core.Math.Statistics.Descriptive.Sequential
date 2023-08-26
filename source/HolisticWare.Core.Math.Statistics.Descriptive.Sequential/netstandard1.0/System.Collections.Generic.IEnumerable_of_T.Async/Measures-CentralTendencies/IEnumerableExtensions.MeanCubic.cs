f99a272ab5d8c8eb67f426717215f40be9ec4f95
moljac <mcvjetko@holisticware.net>
2023-08-26 23:56:14 +0200

    Update IEnumerableExtensions.MeanCubic.cs

--- source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MeanCubic.cs
+++ source/HolisticWare.Core.Math.Statistics.Descriptive.Sequential/netstandard1.0/System.Collections.Generic.IEnumerable_of_T.Async/Measures-CentralTendencies/IEnumerableExtensions.MeanCubic.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Core.Math.Statistics.Descriptive;
-
 namespace Core.Math.Statistics.Descriptive
 {
     /// <summary>
@@ -16,185 +14,214 @@ namespace Core.Math.Statistics.Descriptive
     {
         //==============================================================================================================
         public static
-            double
+            Task<double>
+
                                         MeanCubicAsync
                                         (
                                             this IEnumerable<byte> x
                                         )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                byte x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<short> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<short> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                short x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<ushort> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<ushort> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                ushort x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<int> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<int> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                int x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<uint> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<uint> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                uint x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<long> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<long> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                long x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<ulong> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<ulong> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                ulong x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<float> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<float> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                float x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static double MeanCubicAsync
-                                    (
-                                        this IEnumerable<double> x
-                                    )
+        public static
+            Task<double>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<double> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<double> tcs;
+            tcs = new TaskCompletionSource<double>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            double result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            double sum = 0.0;
-
-            for (int i = 0; i < n; i++)
-            {
-                double x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return System.Math.Pow(sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
-        public static decimal MeanCubicAsync
-                                    (
-                                        this IEnumerable<decimal> x
-                                    )
+        public static
+            Task<decimal>
+                                        MeanCubicAsync
+                                        (
+                                            this IEnumerable<decimal> x
+                                        )
         {
-            int n = x.Count();
+            TaskCompletionSource<decimal> tcs;
+            tcs = new TaskCompletionSource<decimal>();
+            Task.Run
+                    (
+                        () =>
+                        {
+                            decimal result = x.MeanCubic();
+                            tcs.SetResult(result);
+                        }
+                    );
 
-            decimal sum = 0.0m;
-
-            for (int i = 0; i < n; i++)
-            {
-                decimal x_i = x.ElementAt(i);
-                sum += x_i * x_i * x_i;
-            }
-
-            return (decimal) System.Math.Pow((double)sum / n, 1.0 / 3.0);
+            return tcs.Task;
         }
 
         //==============================================================================================================
